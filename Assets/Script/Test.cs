d29d78e00163d9b82eb802273028fdb5730ccadc
Léa <lea.brechet@yahoo.fr>
2020-06-04 14:36:40 +0200

    Add my feature

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("MyFeatures");
     }
 
     // Update is called once per frame
